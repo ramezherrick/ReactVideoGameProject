24f6ab19480c4a0c807cb7b79243238faf73c9e1
Ramez Herrick <ramez.herrick@jato.com>
2023-06-30 11:14:29 -0400

    show a spinner on side panel

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import useGenres, { Genres } from "../hooks/useGenres";
 import useGenericHook from "../hooks/GenericHook";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
 
 const GenreList = () => {
   //   const { genres } = useGenres();
-  const { data } = useGenres();
+  const { data, isLoading, error } = useGenres();
+  if (error) {
+    return null;
+  }
+  if (isLoading) {
+    return <Spinner />;
+  }
   return (
     <List>
       {data.map((g) => (
